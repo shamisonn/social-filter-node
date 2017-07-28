323b0f06b93c6657ace7534fe29791c3d76c1167
shamison <unknown0incident@gmail.com>
2017-07-28 15:09:59 +0900

    add ignore and modify fucntion

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ app.use(bodyParser.json());
 
 const cfg = ini.parseSync('./config.ini');
 
-app.post('/filter', function(req, res) {
+app.post('/filter', (req, res) => {
   const word = social_filter(req.body.text);
-  console.log(word);
-  res.send('{ "result" : "'+word+'" }');
+//	console.log(word);
+  let json = { "result" : word }		
+  res.send(JSON.stringify(json, null , "\t"));
 })
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('social_filter server begin');
 });
 
